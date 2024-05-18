32f268b1bc6ed89f12915d4fec42b7c522dff3c5
Matt Gilbertson <mgilbertsonund@gmail.com>
2024-05-18 18:46:36 -0500

    displayMovie onclick function dispatching movie object to reducer

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -11,15 +11,21 @@ function MovieList() {
     dispatch({ type: 'FETCH_MOVIES' });
   }, []);
 
+  const displayMovie = (movieToDisplay) => {
+    console.log(movieToDisplay);
+    dispatch({ type: 'SET_MOVIE_DETAILS', payload: movieToDisplay })
+  }
+
   return (
     <main>
       <h1>MovieList</h1>
       <section className="movies">
+        {/* loop over array, and for each movie, display on DOM */}
         {movies.map(movie => {
           return (
             <div data-testid='movieItem' key={movie.id}>
               <h3>{movie.title}</h3>
-              <img onClick={() => displayMovie(movie)} src={movie.poster} alt={movie.title}/>
+              <img onClick={(event) => displayMovie(movie)} src={movie.poster} alt={movie.title}/>
             </div>
           );
         })}
